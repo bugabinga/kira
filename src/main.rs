bb2ab77c7e81292ddd6bd3dbcaf3b4daf323ed88
Oliver Jan Krylow <oliver@bugabinga.net>
2019-02-18 13:11:07 +0100

    Nornalize input to percent and smooth apply
    - Input values are expected to be between 0 and 100 and get mapped to
    actual range of values
    - apply target value stepwise over time resulting in a smooth linear
    transition

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,33 @@ fn main() -> Result<()> {
     let max_brightness = backlight.join("max_brightness");
     let brightness = backlight.join("brightness");
     let max_brightness = read_to_string(max_brightness)?;
+    let max_brightness_value = max_brightness.trim().parse().unwrap();
 
     let args: Vec<String> = std::env::args().collect();
     let target: u16 = if args.len() > 1 {
-        args[1].parse().unwrap()
+        let percent: u8 = args[1].parse().unwrap();
+        (max_brightness_value as f32 * percent as f32 / 100.0) as u16
     } else {
-        max_brightness.trim().parse().unwrap()
+        max_brightness_value
     };
 
-    let current_brightness: u16 = read_to_string(&brightness)?.trim().parse().unwrap();
+    let current_brightness_value: u16 = read_to_string(&brightness)?.trim().parse().unwrap();
 
-    dbg!(&current_brightness);
-    for b in current_brightness..target {
-        let mut current_brightness_file = File::create(&brightness)?;
-        current_brightness_file.write_all(&b.to_string().as_bytes())?;
-        current_brightness_file.sync_data()?;
+    if target > current_brightness_value {
+        for b in current_brightness_value..=target {
+            let mut current_brightness_file = File::create(&brightness)?;
+            current_brightness_file.write_all(&b.to_string().as_bytes())?;
+            current_brightness_file.sync_data()?;
+            std::thread::sleep(std::time::Duration::from_nanos(100));
+        }
+    } else if target < current_brightness_value {
+        for b in (target..=current_brightness_value).rev() {
+            let mut current_brightness_file = File::create(&brightness)?;
+            current_brightness_file.write_all(&b.to_string().as_bytes())?;
+            current_brightness_file.sync_data()?;
+            std::thread::sleep(std::time::Duration::from_nanos(100));
+        }
     }
-
-    // TODO delete later
-    let current = read_to_string(&brightness)?;
-    dbg!(&current);
 
     Ok(())
 }
