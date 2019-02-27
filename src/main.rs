da591cb6d43431cdc9c6ebd5973a499d74413208
Oliver Jan Krylow <oliver@bugabinga.net>
2019-02-28 00:37:13 +0100

    Maps error to messages and prints those in case of error
    - more tests
    - more comments
    - print usage
    
    It turns out, writing a __u8__ to a file surprised me.
    Converting it first to a string, then to bytes behaves correctly.
    Converting directly to bytes does not.
    
    Does that mean, I am lucky that Rust encodes Strings the same way Linux
    does, so that it is compatible?

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::read_to_string;
 use std::fs::File;
 use std::io::prelude::*;
+use std::num::ParseIntError;
 use std::path::Path;
 
 /// `kira` allows you to set the display brightness on linux machines with intel graphics cards.
@@ -26,13 +27,60 @@ use std::path::Path;
 ///
 /// Any change in brightness will occur stepwise with a small delay inbetween.
 /// This results in a linear smooth change of brightness over time.
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
+    match kira() {
+        Err(error) => {
+            eprintln!("{}", match_error_to_message(&error));
+            println!("{:?}", &error);
+            let stderr = std::io::stderr();
+            let mut err_handle = stderr.lock();
+            print_usage(&mut err_handle).expect("Could not even write to stderr. Sad life.");
+        }
+        _ => {}
+    }
+}
+
+fn print_usage(writer: &mut dyn Write) -> Result<(), Box<dyn Error>> {
+    write!(
+        writer,
+        "
+usage: kira [+-][percent]
+
+percent must be a number between 0 and 100.
+A prefix of either - oder + is allowed.
+Without a prefix, the brightness gets set to the given percentage.
+With the + prefix, the given percentage gets added to current brightness.
+With the - prefix, the given percentage gets subtracted from current brightness.
+
+You need permission to modify the backlight device in `/sys/class/backlight/`.
+"
+    )?;
+    Ok(())
+}
+
+// For every error that is expected to occur in kira, this method maps a "friendly"
+// explanation text to it.
+fn match_error_to_message(error: &Box<dyn Error>) -> &'static str {
+    if error.is::<ParseIntError>() {
+        "Given percent value needs to be a number between 0 and 100."
+    } else if error.is::<std::io::Error>() {
+        "Could not access the backlight device.
+Does ist exist?
+Usually `/sys/class/backlight/intel_backlight/` or similar.
+Also, do you have permission to edit it?
+On most Linux distributions you need to be part of a special group (video?)."
+    } else {
+        "Oh oh, we've made a fucky wucky..."
+    }
+}
+
+fn kira() -> Result<(), Box<dyn Error>> {
     let backlight = Path::new("/sys/class/backlight/intel_backlight/");
-    let max_brightness = backlight.join("max_brightness");
     let brightness = backlight.join("brightness");
-    let max_brightness = read_to_string(max_brightness)?;
-    let max_brightness_value = max_brightness.trim().parse()?;
-    let min_brightness_value = 0;
+    let max_brightness_value: u16 = read_to_string(backlight.join("max_brightness"))?
+        .trim()
+        .parse()?;
+    let min_brightness_value: u16 = 0;
     let current_value: u16 = read_to_string(&brightness)?.trim().parse()?;
     let args: Vec<String> = std::env::args().collect();
     let target: u16 = if args.len() > 1 {
@@ -62,7 +110,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn write_to_file_and_wait(mut file: &File, value: u16, nanos: u64) -> Result<(), Box<dyn Error>> {
-    file.write_all(&value.to_ne_bytes())?;
+    file.write_all(&value.to_string().as_bytes())?;
     file.sync_data()?;
     std::thread::sleep(std::time::Duration::from_nanos(nanos));
     Ok(())
@@ -78,6 +126,18 @@ fn parse_input_as_percent(input: &str) -> Result<(Option<bool>, u8), Box<dyn Err
     }
 }
 
+/// Calculates the actual brigthness value, given the min-max-range and percent value.
+/// If a signum is given, the percentage value will be added//subtracted to the current
+/// brightness value.
+/// In any case, this method returns the absolute target brigthness value.
+/// signum: Relativizes the `percent` value. `None` means the given `percent` value is
+/// meant to be an absolute target value. `Some(true)` means the target is the current
+/// value added to the percentage. `Some(false)` means the target is the subtraction of
+/// the current value and the given percentage.
+/// percent: Percentage of wanted target value. Expected to be between 0 - 100.
+/// current: the current absolute brightness value (not a percentage).
+/// max: the maximum absolute brightness value.
+/// min: the minimum absolute brigthness value.
 fn calculate_target_value(
     signum: Option<bool>,
     percent: u8,
@@ -118,8 +178,24 @@ mod tests {
     use super::*;
 
     #[test]
+    fn smoke_test_that_usage_is_basically_there() {
+        let mut buffer = vec![];
+        print_usage(&mut buffer).expect("fail");
+        let string = String::from_utf8(buffer).expect("fail");
+        assert!(string.contains("usage"));
+        assert!(string.contains("kira"));
+        assert!(string.contains("percent"));
+        assert!(string.contains("-"));
+        assert!(string.contains("+"));
+        assert!(string.contains("sys"));
+        assert!(string.contains("device"));
+        assert!(string.contains("permission"));
+    }
+
+    #[test]
     fn checkcalculated_target_values() {
         assert_eq!(calculate_target_value(None, 22, 0, 100, 0), 22u16);
+        assert_eq!(calculate_target_value(None, 77, 0, 4438, 0), 3417u16);
         assert_eq!(calculate_target_value(None, 0, 0, 100, 0), 0u16);
         assert_eq!(calculate_target_value(None, 100, 0, 100, 0), 100u16);
         assert_eq!(calculate_target_value(None, 200, 0, 100, 0), 100u16);
@@ -146,6 +222,7 @@ mod tests {
         assert_eq!(calculate_target_value(None, 33, 0, 100, 0), 33u16);
         assert_eq!(calculate_target_value(None, 73, 0, 14687, 999), 10721u16);
     }
+
     #[test]
     fn check_expected_input_values() {
         let (signum, percent) = parse_input_as_percent("+10").unwrap();
